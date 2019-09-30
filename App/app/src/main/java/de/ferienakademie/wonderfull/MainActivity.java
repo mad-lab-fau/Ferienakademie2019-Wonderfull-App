a180311bb93aca47ab63fc13f5df0457992b5ba1
lisann-li <lisann.lieb@t-online.de>
2019-09-30 17:16:43 +0200

    edit menu

--- App/app/src/main/java/de/ferienakademie/wonderfull/MainActivity.java
+++ App/app/src/main/java/de/ferienakademie/wonderfull/MainActivity.java
@@ -79,6 +79,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.menu_sensor:
                 startActivity(new Intent(this, SensorActivity.class));
                 return true;
+            case R.id.menu_plots:
+                startActivity(new Intent(this, Graphen.class));
+                return true;
             default:
                 return false;
         }
