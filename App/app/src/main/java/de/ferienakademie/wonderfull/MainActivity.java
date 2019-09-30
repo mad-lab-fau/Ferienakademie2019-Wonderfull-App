7609528159aa5c8e95174b2dda19cd6753dd2347
VerreH <vere.hinderer@web.de>
2019-09-30 14:52:04 +0200

    main menu finished

--- App/app/src/main/java/de/ferienakademie/wonderfull/MainActivity.java
+++ App/app/src/main/java/de/ferienakademie/wonderfull/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         playClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MainMenuActivity.class));
+                startActivity(new Intent(MainActivity.this, HikeMainscreen.class));
             }
         });
 
