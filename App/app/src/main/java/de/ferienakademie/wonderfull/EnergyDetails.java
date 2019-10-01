281df0881d39a462066864b2e5eb1d911562bc93
VerreH <vere.hinderer@web.de>
2019-10-01 15:09:14 +0200

    energy_details angepasst

--- App/app/src/main/java/de/ferienakademie/wonderfull/EnergyDetails.java
+++ App/app/src/main/java/de/ferienakademie/wonderfull/EnergyDetails.java
@@ -14,25 +14,23 @@ public class EnergyDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_energy_details);
 
-        TextView herzfrequenz_p = (TextView) findViewById(R.id.herzfrequenz_p);
-        herzfrequenz_p.append(" " + "80" + " Hz");
+        /*TextView herzfrequenz_p = (TextView) findViewById(R.id.herzfrequenz_p);
+        herzfrequenz_p.append(" " + "80" + " Hz"); */
 
         TextView hoehenmeter_p = (TextView) findViewById(R.id.hoehenmeter_p);
-        hoehenmeter_p.append(" " + "80" + " mBar");
-
+        hoehenmeter_p.append("80" + " mBar");
 
         TextView strecke_p = (TextView) findViewById(R.id.strecke_p);
-        strecke_p.append(" " + "2,5" + " km");
+        strecke_p.append("2,5" + " km");
 
         TextView schritte_p = (TextView) findViewById(R.id.schritte_p);
-        schritte_p.append(" " + "1042");
+        schritte_p.append("1042");
 
         TextView pause = (TextView) findViewById(R.id.pause);
-        pause.append(" " + "15" + " min");
+        pause.append("15" + " min");
 
-        /*
         TextView zeit_p = (TextView) findViewById(R.id.zeit_p);
-        zeit_p.append(" " + "50" + " min"); */
+        zeit_p.append("50" + " min");
 
         this.setTitle(String.format(energy_title, "45%"));
 
